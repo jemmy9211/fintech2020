3086a82dc671aa74a4e8f8e6b5bfac946ed4ea15
jemmy9211 <jemmy9211@gmail.com>
2020-10-21 00:02:10 +0800

    lar

---
+++ ulb_manager/backend/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+def hello_world(request):
+    return HttpResponse("Hello Nuts!")
+
+
+# Create your views here.
---
+++ ulb_manager/frontend/src/components/app.vue
@@ -0,0 +1,27 @@
+<template>
+<trading-vue :data="this.$data"></trading-vue>
+</template>
+<script>
+
+import TradingVue from 'trading-vue-js'
+import Overlays from 'tvjs-overlays'
+
+export default {
+    name: 'app',
+    components: { TradingVue },
+    data() {
+        return {
+            overlays: Object.values(Overlays),
+            ohlcv: [
+                [ 1551128400000, 33,  37.1, 14,  14,  196 ],
+                [ 1551132000000, 13.7, 30, 6.6,  30,  206 ],
+                [ 1551135600000, 29.9, 33, 21.3, 21.8, 74 ],
+                [ 1551139200000, 21.7, 25.9, 18, 24,  140 ],
+                [ 1551142800000, 24.1, 24.1, 24, 24.1, 29 ],
+                [ 1551146400000, 24.1, 30.1, 24, 30.1, 29 ],
+            ]
+        }
+    }
+}
+
+</script>
